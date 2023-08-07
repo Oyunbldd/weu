7f9de95dcf299d4e7c07015c515f58433cf9a12f
Oyunbldd Ganbldd <oyunbldd@Oyunbldds-MacBook-Pro.local>
2023-08-08 00:20:37 +0800

    firebase init done

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Text('testing'),
+        ),
+      ),
+    );
+  }
+}
