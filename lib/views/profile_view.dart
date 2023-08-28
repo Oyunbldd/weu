43cdd0f2a910d44e9ec44042f3844fb53b8705ae
Oyunbldd Ganbldd <oyunbldd@Oyunbldds-MacBook-Pro.local>
2023-08-29 02:05:03 +0800

    profile read data done

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatefulWidget {
@@ -8,6 +10,35 @@ class ProfileView extends StatefulWidget {
 }
 
 class _ProfileViewState extends State<ProfileView> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  late User user;
+  late DocumentSnapshot snapshot;
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  @override
+  void initState() {
+    user = auth.currentUser!;
+    _phoneController.text = user.phoneNumber!;
+    getData();
+
+    super.initState();
+  }
+
+  void getData() async {
+    //use a Async-await function to get the data
+    final data = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(user.phoneNumber)
+        .get()
+        .then((value) {
+      _userNameController.text = value.data()!['name'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,13 +48,15 @@ class _ProfileViewState extends State<ProfileView> {
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               const Text(
                 'Миний профайл',
                 style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.bold),
               ),
               Divider(),
-              const SizedBox(height: 15),
+              const SizedBox(height: 10),
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 10),
                 decoration: BoxDecoration(
@@ -33,14 +66,14 @@ class _ProfileViewState extends State<ProfileView> {
                   ),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: const TextField(
-                  // controller: _phoneController,
+                child: TextField(
+                  controller: _userNameController,
                   keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     border: InputBorder.none,
-                    hintText: "Утасны дугаар",
+                    hintText: "Name",
                   ),
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontSize: 13,
                     fontWeight: FontWeight.bold,
                   ),
@@ -56,16 +89,64 @@ class _ProfileViewState extends State<ProfileView> {
                   ),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: const TextField(
-                  // controller: _phoneController,
+                child: TextField(
+                  controller: _phoneController,
                   keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     border: InputBorder.none,
-                    hintText: "Email  ",
+                    hintText: "Утасны дугаар",
                   ),
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontSize: 13,
                     fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.25,
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Email",
+                  ),
+                  style: const TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Expanded(child: SizedBox()),
+              Center(
+                child: SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                    child: const Text(
+                      'Гарах',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
                   ),
                 ),
               ),
