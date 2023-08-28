180e7f56600599fae5c5bf7e3f24dd3d1a333f69
Oyunbldd Ganbldd <oyunbldd@Oyunbldds-MacBook-Pro.local>
2023-08-29 02:33:29 +0800

    first demo done

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,6 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cool_alert/cool_alert.dart';
+import 'package:get/get.dart';
+import 'package:weu/routes.dart';
+import 'package:weu/screens/login_screen.dart';
 
 class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
@@ -16,6 +20,8 @@ class _ProfileViewState extends State<ProfileView> {
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
+
+  bool readOnly = true;
 
   CollectionReference users = FirebaseFirestore.instance.collection('users');
 
@@ -51,9 +57,33 @@ class _ProfileViewState extends State<ProfileView> {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const Text(
-                'Миний профайл',
-                style: TextStyle(fontSize: 22.5, fontWeight: FontWeight.bold),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Миний профайл',
+                    style:
+                        TextStyle(fontSize: 22.5, fontWeight: FontWeight.bold),
+                  ),
+                  InkWell(
+                    onTap: () => setState(() {
+                      readOnly = !readOnly;
+                    }),
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1.5, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: const Icon(
+                        Icons.edit,
+                        size: 20,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
               ),
               Divider(),
               const SizedBox(height: 10),
@@ -71,6 +101,7 @@ class _ProfileViewState extends State<ProfileView> {
                 child: TextField(
                   controller: _userNameController,
                   keyboardType: TextInputType.phone,
+                  readOnly: readOnly,
                   decoration: const InputDecoration(
                     border: InputBorder.none,
                     hintText: "Name",
@@ -96,6 +127,7 @@ class _ProfileViewState extends State<ProfileView> {
                 child: TextField(
                   controller: _phoneController,
                   keyboardType: TextInputType.phone,
+                  readOnly: readOnly,
                   decoration: const InputDecoration(
                     border: InputBorder.none,
                     hintText: "Утасны дугаар",
@@ -121,6 +153,7 @@ class _ProfileViewState extends State<ProfileView> {
                 child: TextField(
                   controller: _emailController,
                   keyboardType: TextInputType.phone,
+                  readOnly: readOnly,
                   decoration: const InputDecoration(
                     border: InputBorder.none,
                     hintText: "Email",
@@ -131,12 +164,54 @@ class _ProfileViewState extends State<ProfileView> {
                   ),
                 ),
               ),
+              const SizedBox(height: 25),
+              Visibility(
+                visible: !readOnly,
+                child: Center(
+                  child: SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          backgroundColor: Colors.red.withOpacity(0.75)),
+                      child: const Text(
+                        'Өөрчлөлтийг хадгалах',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
               const Expanded(child: SizedBox()),
               Center(
                 child: SizedBox(
                   width: 200,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      CoolAlert.show(
+                        context: context,
+                        type: CoolAlertType.warning,
+                        title: 'Та гарахдаа итгэлтэй байна уу ?',
+                        titleTextStyle: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        showCancelBtn: true,
+                        onConfirmBtnTap: () {
+                          Navigator.pop(context);
+                        },
+                        onCancelBtnTap: () {},
+                        confirmBtnText: 'Тийм',
+                        cancelBtnText: 'Үгүй',
+                      );
+                    },
                     style: ElevatedButton.styleFrom(
                       elevation: 0.0,
                       shadowColor: Colors.transparent,
